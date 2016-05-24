8667232c582c1600601670f0a5633a500b39bb46
Craig O'Shannessy <craig@perfectlogic.com.au>
2016-05-25 05:48:07 +1000

    Removed unneeded imports and added a comment to PublicXeroClient.java

--- src/main/java/com/rossjourdain/util/xero/PublicXeroClient.java
+++ src/main/java/com/rossjourdain/util/xero/PublicXeroClient.java
@@ -1,19 +1,9 @@
-package xero;
+package com.rossjourdain.util.xero;
 
-import com.rossjourdain.util.xero.ArrayOfInvoice;
-import com.rossjourdain.util.xero.XeroClient;
-import com.rossjourdain.util.xero.XeroClientException;
-import com.rossjourdain.util.xero.XeroClientUnexpectedException;
-import com.rossjourdain.util.xero.XeroXmlManager;
 import net.oauth.OAuthConsumer;
-import net.oauth.OAuthMessage;
-import net.oauth.OAuthProblemException;
-import net.oauth.client.OAuthClient;
-import net.oauth.client.httpclient4.HttpClient4;
 import org.scribe.model.Token;
 import net.oauth.OAuthAccessor;
 
-import java.util.List;
 
 /**
  * FIXME : refactor the whole thing to use plays async WS. https://www.playframework.com/documentation/2.3.x/JavaOAuth
@@ -25,7 +15,7 @@ public class PublicXeroClient extends XeroClient {
 	private final Token accessToken;
 
 	public PublicXeroClient(String endpointUrl, String consumerKey, String consumerSecret, Token accessToken) {
-		super(endpointUrl, consumerKey, consumerSecret, "");
+		super(endpointUrl, consumerKey, consumerSecret, ""); // Last arg is private key, used for private apps
 		this.consumerKey = consumerKey;
 		this.consumerSecret = consumerSecret;
 		this.accessToken = accessToken;
